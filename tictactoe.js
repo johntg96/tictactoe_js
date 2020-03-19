2d2e6d6076e7023acbda9840e5fbbd4c08a204f1
johntg96 <johntg@protonmail.com>
2020-03-18 19:11:11 -0500

    created 3x3 grid w/ divs

---
+++ tictactoe.js
@@ -0,0 +1,11 @@
+const gameBoard = () => {
+	let board = [];
+}
+
+const player = xy => {
+
+}
+
+const block = document.querySelectorAll('.block').forEach(block =>
+	block.addEventListener('click', () => block.style.backgroundColor = 'grey')
+);
