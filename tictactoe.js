53965d8ad1cf9f1cd933fbf0ca8a7761cf2c249e
johntg96 <johntg@protonmail.com>
2020-03-21 11:53:54 -0500

    players moves are added to board array. Logic was made to check if a box has already been marked, and prevents player from marking that box.

--- tictactoe.js
+++ tictactoe.js
@@ -1,11 +1,42 @@
 let currentPlayer = 'X';
+let board = [];
 
-const gameBoard = () => {
-	let board = [];
+const gameBoard = (player, turn) => {
+	board.push(playersTurn(player, turn))
+	console.log(board);
 }
 
-const player = xy => {
+// factory function to create players move
+const playersTurn = (player, turn) => {
+	let xy = player;
+	let mark = turn;
 
+	return {
+		player: xy,
+		turn: mark
+	}
+}
+
+const checkMarked = (id) => {
+	for(var i = 0; i < board.length; i++) {
+		if(board[i].turn == id) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+const markBoard = (id) => {
+	let specificCell = document.querySelector('#' + id);
+	specificCell.innerHTML = '<h1 class="mark" id="mark_' + id + '">' + currentPlayer + '</h1>';
+	gameBoard(currentPlayer, id);
+
+	if(currentPlayer == 'X') {
+		currentPlayer = 'O';
+	} else {
+		currentPlayer = 'X';
+	}
 }
 
 const cell = document.querySelectorAll('.cell').forEach(cell =>
@@ -13,12 +44,14 @@ const cell = document.querySelectorAll('.cell').forEach(cell =>
 );
 
 const clickedCell = (id) => {
-	let specificCell = document.querySelector('#' + id);
-	specificCell.innerHTML = '<h1 id="mark">' + currentPlayer + '</h1>';
+	if(typeof board !== undefined && board.length > 0) {
+		if(checkMarked(id) == true) {
+			alert('already been marked');
+		} else {
+			markBoard(id);
+		}
 
-	if(currentPlayer == 'X') {
-		currentPlayer = 'O';
 	} else {
-		currentPlayer = 'X';
+		markBoard(id);
 	}
 }
